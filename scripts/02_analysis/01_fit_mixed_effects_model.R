2cc00deef4aa075c8b43bed39e8758ff22b54646
jcvdav <juancarlos.villader@gmail.com>
2025-07-23 14:47:59 -0400

    Updated model specification to allow for correlation between slopes

--- scripts/02_analysis/01_fit_mixed_effects_model.R
+++ scripts/02_analysis/01_fit_mixed_effects_model.R
@@ -16,6 +16,7 @@
 pacman::p_load(
   here,
   lme4,
+  equatiomatic,
   modelsummary,
   tidyverse
 )
@@ -29,7 +30,7 @@ yr_eu <- readRDS(here("data/processed/year_eu.rds")) %>%
          C19 = 1 * (period == "C19"))
 
 ## PROCESSING ##################################################################
-model <- lmer(std_rev ~ 0 + MHW + C19 + (0 + MHW | eu_rnpa) + (0 + C19 | eu_rnpa),
+model <- lmer(std_rev ~ 0 + MHW + C19 + (0 + MHW +C19 | eu_rnpa),
               data = yr_eu)
 
 extract_eq(model,
@@ -53,7 +54,8 @@ modelsummary(model,
                              "C19" = "\\mu_{\\gamma_{2i}}",
                              "SD (MHW eu_rnpa)" = "\\sigma_{\\gamma_{1i}}",
                              "SD (C19 eu_rnpa)" = "\\sigma_{\\gamma_{2i}}",
-                             "SD (Observations)" = "\\sigma"),
+                             "SD (Observations)" = "\\sigma",
+                             "Cor (MHW~C19 eu_rnpa)" = "\\rho"),
              output = here("results", "tab", "tab1_main_model.tex"),
              escape = F,
              title = "\\label{tab:main_reg}Regression results for reductions in standard-normalized revenues during Marine heatwaves (MHW) and COVID-19 (C19) periods for 237 economic units in Baja California. Numbers in parentheses indicate standard errors and asterisks show statistical significance. Standard deviations of the random effects are also included at the bottom of the table.")
